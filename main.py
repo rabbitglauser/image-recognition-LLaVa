9e5647ad4a888ab4b3f7a5b900f142acfb83aba8
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-06-30 11:12:13 +0200

    Update chat sequence in ollama API call
    The code has been modified to include an additional user role and question in the ollama API call. Besides asking to describe the image, it also asks about the number of houses in the image. Furthermore, the print statement now also displays the image response.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import ollama
 
 res = ollama.chat(
-    model='llava:13b',
-    messages=[{'role': 'user',
-               'content': 'Describe this image',
-               'image': ['./image1.jpg']}
-              ]
+    model='llava',
+    messages=[
+        {'role': 'user',
+         'content': 'How many houses are in this image?',
+         'image': ['./image1.jpg']},
+
+        {'role': 'user',
+         'content': 'Describe this image',
+         'image': ['./image1.jpg']}
+    ]
 )
 
 print(res['message']['content'])
+print(res['message']['image'])
+
